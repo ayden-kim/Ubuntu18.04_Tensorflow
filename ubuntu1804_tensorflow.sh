07b41f31e894ccae0cd5534c3b5a017833866f9c
DeepAbyss-s <68440516+DeepAbyss-s@users.noreply.github.com>
2020-07-19 19:38:37 +0900

    Update ubuntu1804_tensorflow.sh

--- ubuntu1804_tensorflow.sh
+++ ubuntu1804_tensorflow.sh
@@ -26,3 +26,4 @@ echo " install Tensorflow will start "
 curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 sh Anaconda3-2019.03-Linux-x86_64.sh
 sudo pip3 install tensorflow-gpu
+sudo reboot
