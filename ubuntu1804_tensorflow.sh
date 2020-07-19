b59f81b3c97928221bc1e6bebaa39ad1455e3908
DeepAbyss-s <68440516+DeepAbyss-s@users.noreply.github.com>
2020-07-19 19:29:14 +0900

    Update ubuntu1804_tensorflow.sh

--- ubuntu1804_tensorflow.sh
+++ ubuntu1804_tensorflow.sh
@@ -26,19 +26,3 @@ echo " install Tensorflow will start "
 curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 sh Anaconda3-2019.03-Linux-x86_64.sh
 sudo pip3 install tensorflow-gpu
-python3 && import tensorflow as tf
-hello = tf.constant('hello')
-sess = tf.Session()
-print(sess.run(hello))
-
-echo " if you can see (hello) word tensorflow is successful!"
-
-read -p "Would you like to reboot your system? [y/n]??"
-case $yn in
-	[Yy]* ) echo "  "
-		echo " system will reboot"
-		sudo reboot;;
-	[Nn]* ) echo "  "
-		echo " thank you for use our Service!"
-		exit;;
-esac
